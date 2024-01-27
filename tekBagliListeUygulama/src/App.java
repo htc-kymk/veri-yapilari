b3bf891daf375bc4d0e3218099d3f591f6fcb3a6
htc-kymk <hkaymak967@gmail.com>
2024-01-27 22:57:42 +0300

    Düğüm yazma

---
+++ tekBagliListeUygulama/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Dugum d1=new Dugum();
+        d1.yazdir();
+        Dugum d2=new Dugum(2);
+        d2.yazdir();
+    }
+}
