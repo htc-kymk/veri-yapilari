3f21be2e6b7fb782c1a0d12c9167e1778cd591d9
htc-kymk <hkaymak967@gmail.com>
2024-01-28 19:49:27 +0300

    hazır stack

--- yiginListeUygulama/src/App.java
+++ yiginListeUygulama/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
         yigin y=new yigin();
@@ -13,5 +15,26 @@ public class App {
         System.out.println("yığının en üstü:"+y.peek());
         System.out.println("yığından çekilen"+y.pop());
         System.out.println("yığının en üstü:"+y.peek());
+    
+    
+      Stack<String> myStack=new Stack<>();
+      String dersler[]={"veri","matematik","türkçe"};
+      for(int i=0;i<dersler.length;i++){
+        myStack.push(dersler[i]);
+      }
+
+      while(!myStack.isEmpty()){
+       System.out.println(myStack.pop());                    
+      }
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }
