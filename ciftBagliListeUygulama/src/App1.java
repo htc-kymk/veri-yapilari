94e0b7fa8afc68644e6d1eaeffbb535c1a93cdd0
htc-kymk <hkaymak967@gmail.com>
2024-01-28 16:16:54 +0300

    Çift bağlı liste

---
+++ ciftBagliListeUygulama/src/App1.java
@@ -0,0 +1,13 @@
+public class App1 {
+    public static void main(String[] args) throws Exception {
+       CBList cbl=new CBList();
+       
+       cbl.listele();
+       cbl.arayaEkle(10, 1);
+       cbl.basaEkle(20);
+       cbl.sonaEkle(30);
+       cbl.listele();
+    
+    
+    }
+}
